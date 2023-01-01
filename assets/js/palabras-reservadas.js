c64b2f4616dd49b045cbb1def1b41595c568f6d7
Arturo Saldivar <damonbalam@gmail.com>
2023-01-01 15:05:12 -0500

    cap palabras reservadas

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,18 @@
+// console.log('Hola Mundo!');
+
+
+// let const = 123 /* no se puede usar const porque es una palabra reservada  */
+
+/* Palabras reservadas */
+/* 
+    let
+    const 
+    if
+    else
+    try
+    catch
+    await
+*/
+
+/* Mala practica usar caracteres para el nombre de variables también palabras con Ñ */
+/* Tampoco iniciar con número el nombre de una variable */
