4289623f4d0b4ef83ca12f0c09b6b178b79e1687
Arturo Saldivar <damonbalam@gmail.com>
2023-01-01 14:36:22 -0500

    first commit of 2023

---
+++ assets/js/primitivos.js
@@ -0,0 +1,41 @@
+
+/* String */
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre  = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tía May";
+nombre = `Tía May`;
+console.log( typeof nombre);
+
+
+nombre = 123;
+console.log( typeof nombre);
+
+/*  Boolean */
+let isMarvel = true;
+console.log( isMarvel );
+
+
+/* Number */
+let edad = 27;
+console.log( edad );
+console.log( typeof edad );
+
+
+let superPower;
+console.log( typeof superPower );
+
+let imNull = null;
+console.log( typeof imNull );
+
+
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log(symbol1 === symbol2 );
+console.log(typeof symbol1 );
+console.log(typeof symbol2 );
