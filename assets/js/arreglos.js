9a0d1d8b58a41c9f874f2aaec66a6023b9e7db9d
Arturo Saldivar <damonbalam@gmail.com>
2023-01-03 14:50:53 -0500

    Update arreglos.js
    capitulo de arreglos terminado

--- assets/js/arreglos.js
+++ assets/js/arreglos.js
@@ -1,3 +1,21 @@
-console.log('Hola mundo4!');
+// const arr = [];
 
-/* Hoy hice unos cambios en este archivo rapido  */
+// console.log(arr);
+
+
+let videogames = ['Mario 3','Megaman', 'Metroid' ];
+
+console.log({videogames});
+
+console.log(videogames[0]);
+
+
+let arregloCosas = [true,123,'Fernando',1+2, 
+    function() {},
+    ()=>{},
+    { a: 1},
+    ['X','Megaman','Zero']
+]
+
+// console.log(arregloCosas);
+console.log(arregloCosas[7][2]);
