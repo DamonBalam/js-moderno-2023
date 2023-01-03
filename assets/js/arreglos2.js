c2bed49aa0ed1d01d1679d04920f2885e2e6302b
Arturo Saldivar <damonbalam@gmail.com>
2023-01-03 15:04:55 -0500

    Arreglos pt2
    mas detalles de arreglos

---
+++ assets/js/arreglos2.js
@@ -0,0 +1,34 @@
+let juegos = ['Zelda','Mario','Metroid','Kirby']
+console.log('Largo: ', juegos.length);
+
+let primero = juegos[0];
+let ultimo = juegos[juegos.length-1];
+
+console.log({primero,ultimo});
+
+juegos.forEach( (elemento,indice,arr) => {
+    console.log({elemento,indice,arr});
+})
+
+
+
+let nuevaLongitud = juegos.push("Smash");
+console.log({nuevaLongitud,juegos});
+
+nuevaLongitud = juegos.unshift("Mario Party");
+console.log({nuevaLongitud,juegos});
+
+let juegoBorrado = juegos.pop();
+console.log({juegoBorrado,juegos});
+
+let pos = 1;
+
+let juegosBorrados = juegos.splice(pos, 1);
+
+console.log({juegosBorrados,juegos});
+
+
+let metroid = juegos.indexOf('Metroid');
+console.log({'MetroidIndex':metroid})
+
+
